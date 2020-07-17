3b7cc29992ef261e5f90e6c6b906febcc94a1075
Guillem Ballesteros <guillem.ballesteros.garcia@gmail.com>
2020-07-17 15:51:24 +0000

    Remove comments about the related blog post.

--- FunctionAutomate/PipelinePause/__init__.py
+++ FunctionAutomate/PipelinePause/__init__.py
@@ -26,9 +26,6 @@ from __app__.utilities import utilities
 import azure.functions as func
 from azure.cosmosdb.table.models import Entity
 
-# blog entry also talks about prepopulating pipelines
-# via explicit requests
-
 def get_pipeline_params(req: func.HttpRequest) -> Dict[str, Union[int, str]]:
     pipeline_params = {
         k: utilities.get_param(req, k)
